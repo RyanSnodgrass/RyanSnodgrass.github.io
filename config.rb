97c955740edbbccdb7f13f578a3e58ec974afc45
RyanSnodgrass <res0428@yahoo.com>
2014-06-05 17:58:29 -0400

    after installing gem

--- config.rb
+++ config.rb
@@ -39,6 +39,14 @@
 # configure :development do
 #   activate :livereload
 # end
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  deploy.branch   = "master" # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = "custom-message"      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
